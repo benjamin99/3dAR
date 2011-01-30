d4b4f6bb26b089fbbfc03f90181b999f4b6e2c5d
benjamin99 <cknomber1@gmail.com>
2011-01-30 20:05:10 +0800

    Now can show the camera preview on the main screen ...

--- AndroidProject/src/test/ThreeDAR/beta/ThreeDARMainActivity.java
+++ AndroidProject/src/test/ThreeDAR/beta/ThreeDARMainActivity.java
@@ -2,12 +2,25 @@ package test.ThreeDAR.beta;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class ThreeDARMainActivity extends Activity {
+	
+	private PreviewView _previewView;
+	private FrameLayout _layout;
+	
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        try{
+        	//Setting the _previewView as the main view of the activity:
+            _previewView = new PreviewView( this.getApplicationContext() );
+            _layout = new FrameLayout( this.getApplicationContext() );
+            setContentView(_layout);
+            _layout.addView(_previewView);
+        } 
+        catch(Exception e){}
     }
 }
