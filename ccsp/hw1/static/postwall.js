0eb357fa4d7fa4d56fcf7f3dbef41365b7e396ad
benjamin99 <cknomber1@gmail.com>
2011-04-17 16:35:36 +0800

    ...

--- ccsp/hw1/static/postwall.js
+++ ccsp/hw1/static/postwall.js
@@ -14,4 +14,19 @@ function postMessage() {
 
 
 function updatePostWall() {
+    $.ajax(
+        type: "GET",
+        url: "/fetch",
+        data: {id: g_id },
+        dataType: "xml",
+        timeout: 5000,
+        error: function() { updatePostWall();},
+        success: function(xml) {
+	    var message = "";
+            var id = parseInt($("id",xml).text());
+            if( id > g_id ) {
+                g_id = id;
+            }
+        }
+    );
 }
