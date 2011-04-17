b21300f925b09e104b01e13a54a159edd9749d08
benjamin99 <cknomber1@gmail.com>
2011-04-17 17:01:20 +0800

    now can use ajax for fetching messages ...

--- ccsp/hw1/static/postwall.js
+++ ccsp/hw1/static/postwall.js
@@ -1,3 +1,5 @@
+var g_id = 0;
+
 function postMessage() {
     
     $.post(
@@ -14,7 +16,7 @@ function postMessage() {
 
 
 function updatePostWall() {
-    $.ajax(
+    $.ajax({
         type: "GET",
         url: "/fetch",
         data: {id: g_id },
@@ -22,11 +24,31 @@ function updatePostWall() {
         timeout: 5000,
         error: function() { updatePostWall();},
         success: function(xml) {
-	    var message = "";
+	    var info = "";
             var id = parseInt($("id",xml).text());
             if( id > g_id ) {
                 g_id = id;
+                $("message", xml).each( function(i){
+		    var message = $("message", xml).get(i);
+                    var content = decodeURI($("content", message).text()).replace(/</g, "&lt;").replace(/>/g, "&gt;");
+                    /*if (($.browser.mozilla || $.browser.opera) && content.length > LENGTH) {
+                        var str = "";
+                        while (content.length > LENGTH) {
+                           str += content.substr(0, LENGTH) + "<br />";
+                           content = content.substr(LENGTH, content.length);
+                        }
+                        content = str + content;
+                    }*/
+                    info += "<b>" + $("author", message).text() +
+                            "</b> @ " + $("time", message).text() + ":<blockquote><pre>" +
+                            content + "</pre></blockquote>";
+
+                });
             }
+            if(info) {
+                $('#info').prepend(info);
+            }
+            updatePostWall();
         }
-    );
+    });
 }
