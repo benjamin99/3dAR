63f5d645532db066b62b0ee9f44a14ccffd50f00
Wantoto <wantoto@device-46a310.(none)>
2011-06-03 22:41:07 +0800

    now fb-login can work w/ jQtouch

--- ccsp_hw/hw3/main.py
+++ ccsp_hw/hw3/main.py
@@ -66,7 +66,7 @@ class MainHandler(BaseHandler):
         user = self.current_user
         path = os.path.join(
 			os.path.dirname(__file__), 'templates', 
-			'baseJqt.html' )
+			'mainJqt.html' )
 
 	self.response.out.write( template.render(path,{  'user':user }) )
 
